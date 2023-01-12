4ea62bda88bb6f3790b9aee5dae13968b5a25ee5
neithalrajpurohit <rajpurohitneithal@gmail.com>
2023-01-12 10:50:03 +0530

    completed_ageCalculator

--- Age-Calculator/app.js
+++ Age-Calculator/app.js
@@ -1,17 +1,41 @@
-const container;
-const error;
+// const container;
+// const error;
 
-function getAge(){
-    
-    
+let inputEl = document.querySelector("#dob");
+let resultEl = document.querySelector(".desc");
+let yearsEL = document.querySelector("#years");
+let monthsEl = document.querySelector("#months");
+let daysEl = document.querySelector("#days");
+
+inputEl.addEventListener("change", (e) => {
+  getAge(e);
+  displayAll(e);
+});
+function getAge(e) {
+  let date = new Date(e.target.value).getFullYear();
+  let curr = new Date().getFullYear();
+  let res = curr - date - 1;
+  resultEl.textContent = `Your age is ${res}`;
+  return res;
+}
+function displayAll(e) {
+  let date = new Date(e.target.value);
+
+  let year = getAge(e);
+
+  //   let currYear = new Date().getFullYear();
+
+  let month = date.getMonth();
+  let currMonth = new Date().getMonth();
+
+  let days = date.getDay();
+  let currDay = new Date().getDay();
+
+  yearsEL.textContent = `${year} `;
+  monthsEl.textContent = `${Math.abs(currMonth - month - 1)} `;
+  daysEl.textContent = `${currDay - days - 1} `;
 }
 
-function calcAge(){ 
-    
-}
+function calcAge() {}
 
-function displayAge(){
-    
-    
-}
-
+function displayAge() {}
