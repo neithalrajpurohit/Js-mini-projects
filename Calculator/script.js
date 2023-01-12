f19acd2ad94cc37a87840ad50462368a36e16502
neithalrajpurohit <rajpurohitneithal@gmail.com>
2023-01-12 12:04:10 +0530

    completed_calculator

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,14 +1,63 @@
+let stack = [];
+let operators = [];
 
-function insert(num){
+let inputEl = document.querySelector(".textview");
+
+function insert(num) {
+  if (!isNaN(num)) {
+    stack.push(num);
+    inputEl.value = num;
+  } else if (
+    num === "+" ||
+    num === "-" ||
+    num === "*" ||
+    num === "/" ||
+    num === "%"
+  ) {
+    operators.push(num);
+  } else if (num === "=") {
+    equals();
   }
-  
-  function equals(){
-    
-      
+  num = 0;
+}
+
+function equals() {
+  let firstDig = stack[0];
+  let secondDig = stack[1];
+  let res = 0;
+  switch (operators[0]) {
+    case "+": {
+      res = firstDig + secondDig;
+      break;
+    }
+    case "-": {
+      res = firstDig - secondDig;
+      break;
+    }
+    case "*": {
+      res = firstDig * secondDig;
+      break;
+    }
+    case "/": {
+      res = firstDig / secondDig;
+      break;
+    }
+    case "%": {
+      res = firstDig % secondDig;
+      break;
+    }
   }
-  
-  function clean(){
-  }
-  
-  function back(){
-  }
+  console.log(res);
+  stack = [];
+  operators = [];
+  inputEl.value = res;
+  res = 0;
+}
+
+function clean() {
+  inputEl.value = 0;
+  stack = [];
+  operators = [];
+}
+
+function back() {}
